8e4c433ae5542653a6b7f686a3ab8227523a0f51
Noëlle Anthony <noelle.d.anthony@gmail.com>
2017-05-31 10:54:58 -0400

    More improvements

--- BotElWeather.py
+++ BotElWeather.py
@@ -10,12 +10,12 @@ class BotElWeather():
     def getWeather(self, passed, requester=None, leader=None):
         if leader == None: leader = "\""
         if requester == None: requester = "Somebody"
-        if len(passed) == 1 and self.isInt(passed[0]):
-            arg = passed[0]
+        if self.isInt(passed[0]):
+            arg = passed
         else:
-            arg = "+".join(passed.split(" "))
+            arg = "~" + "+".join(passed.split(" "))
         self.prnt.output("{} requested the weather for {}".format(requester, passed))
-        url = "http://wttr.in/{}?1nT".format(arg)
+        url = "http://wttr.in/{}?0T".format(arg)
         self.prnt.output("Sending request: {}".format(url))
         req = urllib.request.Request(
             url,
@@ -42,7 +42,7 @@ class BotElWeather():
         text = re.sub("\/[^\/]+\/","",text)
         text = re.sub("\[[^\]]+\]","",text)
         text = re.sub("%r%rNew.+$","",text)
-        text = re.sub(r"[┌─┐┴└┘├┼┤┬]","%b",text)
+        #text = re.sub(r"[┌─┐┴└┘├┼┤┬]","%b",text)
         return text
 
     def isInt(self, s):
